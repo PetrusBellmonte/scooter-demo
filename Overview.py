d7ce8b07f5eaa93d77f094ce095cb02296b593d4
PetrusBellmonte <petrusbellmonte@gmail.com>
2025-12-30 16:34:59 +0100

    Final Commit

--- Overview.py
+++ Overview.py
@@ -10,5 +10,7 @@ Use the sidebar to navigate between the following pages:
 - **Overview**: You are already here!
 - **Data Exploration**: Explore the scooter dataset, visualize key features, and understand data distributions (and the many errors).
 - **Model Training, Optimization & Evaluation**: Get an overview over the model, its results and evaluate your own dataset.
+
+The full code can be found on [GitHub](https://github.com/PetrusBellmonte/scooter-demo).
 """)
 
